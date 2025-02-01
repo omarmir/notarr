1fffa8295474236e1925e820727a6f36d5b84ba8
Omar Mir <omarmir@gmail.com>
2025-02-01 00:47:14 -0500

    Implemented Muldown

---
+++ layouts/Default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-100 text-gray-900">
+    <div class="container mx-auto p-8">
+      <slot></slot>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Milkdown from "~/components/MilkdownEditor.vue"
+import { MilkdownProvider } from "@milkdown/vue"
+import "@milkdown/crepe/theme/common/style.css"
+import "@milkdown/crepe/theme/frame.css"
+</script>
+
+<template>
+  <MilkdownProvider>
+    <Milkdown />
+  </MilkdownProvider>
+</template>
