07067ee719b96d5233cda14969f4f90e63fb27b2
Omar Mir <omarmir@gmail.com>
2025-02-21 14:28:29 -0500

    fix: using proper note name this is mostly ignored but even so best to pass correct name

--- pages/[notebook]/[note].vue
+++ pages/[notebook]/[note].vue
@@ -104,8 +104,8 @@ const saveFile = async (markdownText: string) => {
   const blob = new Blob([markdownText], { type: 'text/markdown' })
 
   const formData = new FormData()
-  formData.append('file', blob, 'example.md') // The file to upload
-  formData.append('filename', 'example.md') // The filename to use when saving
+  formData.append('file', blob, `${note}.md`) // The file to upload
+  formData.append('filename', `${note}.md`) // The filename to use when saving
 
   try {
     await $fetch(`/api/${notebook}/${note}`, { method: 'PATCH', body: formData })
